0f0aedd4e4f75ae51e5379f902ee20fa977d1e6d
Rafael Kazuhito Vidal Beltrão <rafakazuhito@gmail.com>
2023-12-04 20:54:16 -0300

    primeira etapa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -8,5 +9,20 @@ const jump = () => {
     
     }, 500);
 }
+
+const loop=setInterval(()=>{
+
+    const pipePosition= pipe.offsetLeft;
+    const marioPosition= + window.getComputedStyle(mario).bottom.replace('px','');
+
+    console.log(marioPosition);
+
+    if(pipePosition<120){
+        pipe.style.animation='none';
+        pipe.style.left= `${pipePosition}px`;
+
+    }
+
+},10);
 
 document.addEventListener('keydown', jump);
